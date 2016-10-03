1fd11ef4372a1e6f14e2dbc2a9c9f26f5df934a3
btow <btow@yandex.ru>
2016-10-03 23:01:20 +0500

    Figure - created

--- src/io/hexlet/xo/model/Figure.java
+++ src/io/hexlet/xo/model/Figure.java
@@ -1,7 +1,16 @@
 package io.hexlet.xo.model;
 
-/**
- * Created by samsung on 03.10.2016.
- */
 public enum Figure {
+    X("X"), O("O");
+
+    private final String figure;
+
+    Figure (final String figure) {
+        assert figure != null;
+        this.figure = figure;
+    }
+
+    public String getFigure(){
+        return figure;
+    }
 }
