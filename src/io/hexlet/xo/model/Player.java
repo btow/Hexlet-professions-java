7a1ccb07d2a123984924e45d9b259939263d0dfb
btow <btow@yandex.ru>
2016-10-03 23:15:13 +0500

    Player - created

---
+++ src/io/hexlet/xo/model/Player.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.model;
+
+/**
+ * Created by btow on 03.10.2016.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(final String name, final Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
